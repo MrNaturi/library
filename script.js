c0e0c431f8d4b2ee02c095722d26cf84aaed6784
Ofuje <daniaemmanuel06@gmail.com>
2025-01-21 17:46:41 +0100

    working on adding books to the library

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 const myLibrary = ["book", "books", "damn"];
 const library = document.getElementById("library")
+const newBkBtn = document.getElementById("newbook")
+const newForm = document.getElementById("newbookform")
+const dialog = document.getElementById("dialog")
+const submitBtn = document.getElementById("submitBtn")
 
-function Book() {
-  // the constructor...
+function Book(title, author,pages, read) {
+  this.title = title
+  this.author = author
+  this.pages = pages
+  this.read = read
+  this.update = function() {
+    myLibrary.push({
+      title:  this.title,
+      author: this.author,
+      pages: this.pages,
+      read: this.read,
+
+    })
+  }
 }
 
 function addBookToLibrary() {
 
+}
+newBkBtn.onclick = () => {
+    dialog.showModal();
+}
+
+submitBtn.onclick = (event) => {
+    event.preventDefault();
+    dialog.close
 }
 
 myLibrary.forEach(el => {
