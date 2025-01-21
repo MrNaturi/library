58420414dee7b0a38d64be14a3cfbf4a0e971b9f
Ofuje <daniaemmanuel06@gmail.com>
2025-01-21 18:46:54 +0100

    completed all the functionality

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
-const myLibrary = ["book", "books", "damn"];
+const myLibrary = [{
+    title: "Sufficiently Advanced Magic",
+    author: "Andrew Rowe",
+    pages: "478",
+    read: "Read",
+},
+{
+    title: "On the shoulders of Titan",
+    author: "Andrew Rowe",
+    pages: "478",
+    read: "Read",
+}, 
+];
 const library = document.getElementById("library")
 const newBkBtn = document.getElementById("newbook")
 const newForm = document.getElementById("newbookform")
 const dialog = document.getElementById("dialog")
 const submitBtn = document.getElementById("submitBtn")
+const selectEl = dialog.querySelector("select")
+const author = document.getElementById("author")
+const title = document.getElementById("Title")
+const pagenumber = document.getElementById("pagenumber")
 
 function Book(title, author,pages, read) {
   this.title = title
@@ -16,23 +32,45 @@ function Book(title, author,pages, read) {
       author: this.author,
       pages: this.pages,
       read: this.read,
-
     })
   }
 }
 
-function addBookToLibrary() {
+function addLibrary (){
+    myLibrary.forEach(el => {
+        library.innerHTML += `<div>
+        <h1>${el.title}</h1>
+        <p>${el.author}</p>
+        <p>${el.pages}</p>
+        <btn class="toggleRead">${el.read}</btn>
+    </div>`
+    })
+} 
 
-}
+
+
 newBkBtn.onclick = () => {
     dialog.showModal();
 }
 
 submitBtn.onclick = (event) => {
     event.preventDefault();
-    dialog.close
+    const formData = [
+        author.value,
+        title.value,
+        pagenumber.value,
+        selectEl.value,
+    ];
+    const serializedData = JSON.stringify(formData);
+    dialog.close(serializedData);
 }
 
-myLibrary.forEach(el => {
-    library.innerText += ` ${el}`
-})
+dialog.addEventListener("close", () => {
+    const resultArray = JSON.parse(dialog.returnValue);
+   const newBook = new Book(resultArray[0], resultArray[1], resultArray[2], resultArray[3]) 
+   newBook.update()
+   library.innerHTML = ""
+   addLibrary()
+});
+
+addLibrary()
